ca7407f7cb54f5569203799a434c2ab49ddfcae7
mohansree <msreerekha2002@gmail.com>
2026-03-13 06:11:16 +0000

    dev: add vendor login api

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -117,3 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+"""Django REST framework configuration."""
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
